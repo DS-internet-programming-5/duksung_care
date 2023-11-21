27662568619c33aecb1aa1a9fb09fdc782aa3456
chaeeunnn <90565473+chaeeunnn@users.noreply.github.com>
2023-11-22 04:18:59 +0900

    각 app의 기본 모델 작성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, nickname="", date_of_birth=None, phone=None, profileImg=None, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            date_of_birth=date_of_birth,
+            phone=phone,
+            profileImg=profileImg,
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            email,
+            username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.nickname = 'admin'
+        user.save(using=self._db)
+        return user
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(max_length=30, unique=True)
+    date_of_birth = models.DateField(null=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    phone = PhoneNumberField(null=True)
+    profileImg = models.ImageField(upload_to='user/profile/%Y/%m/%d/', blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
--- duksung_care/urls.py
+++ duksung_care/urls.py
@@ -12,11 +12,13 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('hospital/', include('hospital.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
+    path('hospital/', include('hospitals.urls')),
     path('admin/', admin.site.urls),
 ]
