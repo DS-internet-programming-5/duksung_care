13c50bac31ebc6b1395c2da5217a0a877c310c00
rudfla0312 <142518978+rudfla0312@users.noreply.github.com>
2023-11-27 23:23:48 +0900

    Feat: login error message 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,38 @@
 from django import forms
-from .models import User
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.hashers import check_password
 
 User=get_user_model()
 
 class SignupForm(UserCreationForm):
     email=forms.EmailField(label="이메일")
-
     class Meta(UserCreationForm):
         model=User
-        fields=['email','nickname','date_of_birth','phone','username','profileImg']
+        fields=('email','nickname','date_of_birth','phone','username','profileImg')
 
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', })
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                self.add_error('email', '이메일이 존재하지 않습니다.')
+                return
+
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호가 틀렸습니다.')
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from accounts.models import User
-from django.contrib import auth
+from .forms import SignupForm, LoginForm
+from django.contrib.auth import authenticate, login
+from django.views.generic import FormView
+
 
 @login_required
 def user_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
     return render(request, 'accounts/user_detail.html', {'user': user})
+
 
 @login_required
 def modify(request):
@@ -23,9 +27,10 @@ def modify(request):
     else:
         return render(request, 'accounts/modify.html')
 
+
 def SignupPage(request):
-    form_class=SignupPage
     if request.method == 'POST':
+        form_class = SignupForm(request.POST)
         user = User.objects.create_user(
             email=request.POST.get('email'),
             password=request.POST.get('password'),
@@ -35,20 +40,28 @@ def SignupPage(request):
             phone=request.POST.get('phone'),
             profileImg=request.FILES.get('profileImg'),
         )
-        auth.login(request, user)
+        login(request, user)
         return redirect('/')
-    return render(request, 'signup.html')
+    return render(request, 'accounts/signup.html')
 
 
-def LoginPage(request):
-    if request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
-        user = auth.authenticate(request, email=email, password=password)
+# @method_decorator(logout_message_required, name='dispatch')
+class LoginPage(FormView):
+    template_name = 'accounts/login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, email=email, password=password)
+
         if user is not None:
-            auth.login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html', {'error': '이메일 또는 비밀번호가 틀렸습니다.'})
-    else:
-        return render(request, 'login.html')
+            self.request.session['email'] = email
+            login(self.request, user)
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, '이메일 또는 비밀번호가 일치하지 않습니다.')
+        return super().form_invalid(form)
