f43fd25778f5f8c716f16a2abee572f83278d272
chaeeunnn <90565473+chaeeunnn@users.noreply.github.com>
2023-12-02 18:22:54 +0900

    Fix: 메인페이지2 카테고리 전부 보이게 수정

--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
 # main/views.py
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from hospitals.models import Category
 
 def main_page1_view(request):
     context = {'my_variable': 'Hello from Main Page 1!'}
     return render(request, 'main/main_page1.html', context)
 
 def main_page2_view(request):
-    context = {'my_variable': 'Hello from Main Page 2!'}
+    categories = Category.objects.all()
+    context = {'my_variable': 'Hello from Main Page 2!', 'categories': categories}
     return render(request, 'main/main_page2.html', context)
